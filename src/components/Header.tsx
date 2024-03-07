7fbe5a264ebd465660e405694c163f50f722ea2f
eva diaz <dev.evada@github.com>
2024-03-07 17:05:11 +0100

    se corrige la revisión del header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,17 @@
 import React from "react";
-// import {imgHostal} from './src/assets/imgHostal.pmg'
 
 export const Header: React.FC = () => {
 
-    return (
-        <div>
-              <div className="flex flex-row justify-center items-center gap-10 w-full h-24  text-orange-800">
-                <div>Habitaciones</div>
-                <div>Ubicación</div>
-                <div className="flex flex-col items-center gap-1">
-                    <img className="w-12 h-12" src="src/assets/imgHostal.png" alt="logotipo" />
-                    <strong >HOSTAL ANA</strong>
-                </div>
-                <div>Galería</div>
-                <div>Contactos</div>
-              </div>
+    return (        
+        <div className="flex flex-row justify-center items-center gap-10 w-full h-24  text-orange-800">
+            <div>Habitaciones</div>
+            <div>Ubicación</div>
+            <div className="flex flex-col items-center gap-1">
+                <img className="w-12 h-12" src="src/assets/imgHostal.png" alt="logotipo" />
+                <span className="font-bold">HOSTAL ANA</span>
+            </div>
+            <div>Galería</div>
+            <div>Contactos</div>
         </div>
     )
 }
