75c3018b797666721522ab272c67b6d2a95b9ccf
eva diaz <dev.evada@github.com>
2024-03-19 16:27:12 +0100

    se crea un objeto rooms y se renombra el componente Card

--- src/components/RoomCard.tsx
+++ src/components/RoomCard.tsx
@@ -1,19 +1,18 @@
 import React from "react";
+import { Room } from "./room.vm"
 
 interface Props {
-    name: string,
-    description: string,
-    img: string
+    room: Room
 }
 
-export const Card: React.FC<Props> = (props) => {
-    const {name, description, img} = props;
+export const RoomCard: React.FC<Props> = (props) => {
+    const { room } = props;
 
     return (
         <div className=" bg-slate-50 w-10/12 h-full flex flex-row p-10">
             <div className="flex flex-col gap-6 w-3/5">
-                <h3 className="text-secundary uppercase">{name}</h3>
-                <p className="text-grey-ligth">{description}
+                <h3 className="text-secundary uppercase">{room.name}</h3>
+                <p className="text-grey-ligth">{room.description}
 
                 
                 </p>
@@ -25,7 +24,7 @@ export const Card: React.FC<Props> = (props) => {
                 </ul>
                 <button type="submit" className="text-sm font-medium bg-yellow-700 border-slate-300 text-white px-4 py-2 rounded">Ver Habitación</button>  
             </div>
-            <img src={img} alt="image-room" className="w-2/5 ml-6"/>
+            <img src={room.img} alt="image-room" className="w-2/5 ml-6"/>
         </div>
     )
 }
