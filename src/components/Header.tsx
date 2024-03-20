d8ae5895564e5bdb6b973e2236f866acccb7f6d1
eva diaz <dev.evada@github.com>
2024-03-20 11:21:21 +0100

    mejora del header añaiendo un navBar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,16 +4,18 @@ import { switchroutes } from "../route/routes";
 
 export const Header: React.FC = () => {
 
-    return (        
-        <header className="flex flex-row justify-center items-center gap-10 w-full h-24  text-primary">
-            <Link to={switchroutes.room}>Habitaciones</Link>
-            <Link to={switchroutes.location}>Ubicación</Link>
-            <Link to={switchroutes.root} className="flex flex-col items-center gap-1">
-                <img className="w-12 h-12" src="src/assets/imgHostal.png" alt="logotipo" />
-                <span className="font-bold">HOSTAL ANA</span>
-            </Link>
-            <Link to={switchroutes.gallery}>Galería</Link>
-            <Link to={switchroutes.contact}>Contactos</Link>
-        </header>
+    return (  
+        <header>
+            <nav className="flex flex-row justify-center items-center gap-10 w-full h-24  text-primary">
+                <Link to={switchroutes.room}>Habitaciones</Link>
+                <Link to={switchroutes.location}>Ubicación</Link>
+                <Link to={switchroutes.root} className="flex flex-col items-center gap-1">
+                    <img className="w-12 h-12" src="src/assets/imgHostal.png" alt="logotipo" />
+                    <span className="font-bold">HOSTAL ANA</span>
+                </Link>
+                <Link to={switchroutes.gallery}>Galería</Link>
+                <Link to={switchroutes.contact}>Contactos</Link>
+            </nav>
+        </header>      
     )
 }
