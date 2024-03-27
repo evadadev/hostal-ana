dd39a5c8688386cc7c20edf8de64933719a4439a
eva diaz <dev.evada@github.com>
2024-03-27 15:15:42 +0100

    modal aun con cambios y mejoras que hacerle

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -6,37 +6,78 @@ import images from '../assets/images'
 
 export const GaleriaPage: React.FC = () => {
     const [ isModalOpen, setIsModalOpen ] = React.useState(false);
+    const [ imgModal, setImgModal ] = React.useState("");
 
-    const openModal = () => {
+    const openModal = (image: string) => { 
+        setImgModal(image)
         setIsModalOpen(true)
     };
 
-    const closeModal = () => {
-        setIsModalOpen(false)
-    };
+    const closeModal = () => setIsModalOpen(false);
+
+    const photos = [
+        {
+            image: images.habitacionAna,
+            id: '1'
+        },
+        {
+            image: images.habitacionAna2,
+            id: '2'
+        },
+        {
+            image: images.habitacionJose,
+            id: '3'
+        },
+        {
+            image: images.habitacionJose2,
+            id: '4'
+        },
+        {
+            image: images.habitacionEva,
+            id: '5'
+        },
+        {
+            image: images.habitacion3,
+            id: '6'
+        },
+        {
+            image: images.habitacionDoble,
+            id: '7'
+        },
+        {
+            image: images.habitacionDoble2,
+            id: '8'
+        },
+        {
+            image: images.terraza,
+            id: '9'
+        },
+        {
+            image: images.patio,
+            id: '10'
+        },
+        {
+            image: images.aseo,
+            id: '11'
+        }
+    ]
 
     return (
         <Layout>
             <WrapperPage>
                 <HeaderPage name="Galería"/>
                 <p className="text-grey-ligth w-9/12 text-justify">En nuestro hostal encontrarás todas las comodidades y entorno familiar, contando con una terraza donde podrás ver las fantasticas vistas a uno de los pueblos más bonitos y con encanto de todo el sur de España</p>
-                <div className="grid grid-cols-3 gap-4 w-9/12 place-self-auto mt-5">
-                    {isModalOpen && (  
-                        <div className="fixed w-full h-full " onClick={closeModal}> 
-                            <img className="hover:opacity-50" onClick={openModal} src={images.habitacionAna} alt="habitacion individual" />                
-                        </div>                  
-                        )}
-                    <img className="hover:opacity-50" src={images.habitacionAna2} alt="habitacion individual" />         
-                    <img className="hover:opacity-50" src={images.habitacionJose} alt="habitacion individual" />                    
-                    <img className="hover:opacity-50" src={images.habitacionJose2} alt="habitacion individual" />                    
-                    <img className="hover:opacity-50" src={images.habitacionEva} alt="habitacion individual" />                    
-                    <img className="hover:opacity-50" src={images.habitacion3} alt="habitacion individual" />                    
-                    <img className="hover:opacity-50" src={images.habitacionDoble} alt="habitacion individual" />                    
-                    <img className="hover:opacity-50" src={images.habitacionDoble2} alt="habitacion individual" />                    
-                    <img className="hover:opacity-50" src={images.terraza} alt="habitacion individual" />                    
-                    <img className="hover:opacity-50" src={images.patio} alt="habitacion individual" />                    
-                    <img className="hover:opacity-50" src={images.aseo} alt="habitacion individual" />                   
-                </div>
+                <div className="grid grid-cols-3 gap-4 w-9/12 place-self-auto mt-5"> 
+                {photos.map(photo => (<img src={photo.image} className="hover:opacity-50" onClick={() => openModal(photo.image)}/>))}
+                {isModalOpen && (
+                    <div className="fixed inset-0 flex items-center justify-center z-10 bg-black opacity-80" onClick={closeModal} >
+                            <img
+                            className="flex w-[50%] " 
+                            src={imgModal} 
+                            alt="habitacion individual" />
+                    </div>
+                )}      
+                </div>                  
             </WrapperPage>
         </Layout>
     )
