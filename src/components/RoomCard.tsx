c476ad21cca54a0f17b0c9be2b61e45817c7cc52
eva diaz <dev.evada@github.com>
2024-04-01 12:30:10 +0200

    se elimina los botones y se añaden los iconos

--- src/components/RoomCard.tsx
+++ src/components/RoomCard.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Room } from "./room.vm"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faWifi, faRadio, faTv, faFan, faPhone, faBathtub } from "@fortawesome/free-solid-svg-icons";
+
 
 interface Props {
     room: Room
@@ -11,18 +14,35 @@ export const RoomCard: React.FC<Props> = (props) => {
     return (
         <div className=" bg-slate-50 w-10/12 h-full flex flex-row p-10">
             <div className="flex flex-col gap-6 w-3/5">
-                <h3 className="text-secundary uppercase">{room.name}</h3>
-                <p className="text-grey-ligth">{room.description}
-
-                
+                <h3 className="text-secundary uppercase font-bold">{room.name}</h3>
+                <p className="text-grey-ligth">{room.description}                
                 </p>
-                <ul className="text-grey-ligth">
-                    <li>Wi-Fi gratis</li>
-                    <li>hilo musical</li>
-                    <li>Televisión por satélite</li>
-                    <li>Aire acondicionado</li>
-                </ul>
-                <button type="submit" className="text-sm font-medium bg-yellow-700 border-slate-300 text-white px-4 py-2 rounded">Ver Habitación</button>  
+                <div className="text-grey-ligth grid grid-cols-2 gap-5">
+                    <div className="flex flex-row gap-4 text-secundary">
+                        <FontAwesomeIcon icon={faWifi} />
+                        <p> Internet Wifi gratis</p>
+                    </div>
+                    <div className="flex flex-row gap-4 text-secundary">
+                    <FontAwesomeIcon icon={faRadio} />
+                        <p>Hilo musical</p>
+                    </div>
+                    <div className="flex flex-row gap-4 text-secundary">
+                        <FontAwesomeIcon icon={faTv} />
+                        <p>Televisión por satélite</p>
+                    </div>
+                    <div className="flex flex-row gap-4 text-secundary">
+                        <FontAwesomeIcon icon={faFan} />
+                        <p>Aire acondicionado</p>
+                    </div>
+                    <div className="flex flex-row gap-4 text-secundary">
+                        <FontAwesomeIcon icon={faPhone} />
+                        <p>Teléfono</p>
+                    </div>
+                    <div className="flex flex-row gap-4 text-secundary">
+                        <FontAwesomeIcon icon={faBathtub} />
+                        <p>Baño privado</p>
+                    </div>
+                </div>
             </div>
             <img src={room.img} alt="image-room" className="w-2/5 ml-6"/>
         </div>
