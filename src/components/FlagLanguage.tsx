3155eb00f810e40eadc872e72bad319de93ffc30
eva diaz <dev.evada@github.com>
2024-04-10 11:01:25 +0200

    se cambia el nombres de las banderas de los idiomas

--- src/components/FlagLanguage.tsx
+++ src/components/FlagLanguage.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { ES, GB } from "country-flag-icons/react/1x1";
 import i18n from "../i18n";
+import { useTranslation } from "react-i18next";
 
 export const FlagLanguage: React.FC = () => {
     const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
+    const { t } = useTranslation();
 
     const changeLanguage = (lang: string) => {
         i18n.changeLanguage(lang);
@@ -14,10 +16,10 @@ export const FlagLanguage: React.FC = () => {
                 <div className="absolute z-20 right-4 top-4 bg-white border border-gray-200 shadow-lg rounded">
                     <button className="flex items-center gap-2 px-4 py-2 text-gray-800 hover:bg-gray-100"  onClick={() => changeLanguage('es')} disabled={currentLanguage === 'es'}>
                         <ES className="w-4 h-4" />
-                        Español</button>
+                        {t('español')}</button>
                     <button className="flex items-center gap-2 px-4 py-2 text-gray-800 hover:bg-gray-100" onClick={() => changeLanguage('en')} disabled={currentLanguage === 'en'}>
                         <GB  className="w-4 h-4"/>
-                        English</button>
+                        {t('ingles')}</button>
                 </div>
     )
 }
